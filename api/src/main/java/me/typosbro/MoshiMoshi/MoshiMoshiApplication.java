6563263c74f3b9a205f96801b32b7ade7bf94b78
Azizbek <khasanjonovich@gmail.com>
2023-03-27 02:00:32 +0900

    Spring init

---
+++ api/src/main/java/me/typosbro/MoshiMoshi/MoshiMoshiApplication.java
@@ -0,0 +1,13 @@
+package me.typosbro.MoshiMoshi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoshiMoshiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoshiMoshiApplication.class, args);
+	}
+
+}
---
+++ api/src/test/java/me/typosbro/MoshiMoshi/MoshiMoshiApplicationTests.java
@@ -0,0 +1,13 @@
+package me.typosbro.MoshiMoshi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoshiMoshiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
