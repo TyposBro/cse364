dda45db7efc884b7c52e8498e327ff6d73a765ac
Azizbek <khasanjonovich@gmail.com>
2023-04-04 21:26:10 +0900

    CommandLineRunner

--- src/main/java/me/typosbro/moshimoshi/MoshiMoshiApplication.java
+++ src/main/java/me/typosbro/moshimoshi/MoshiMoshiApplication.java
@@ -1,7 +1,11 @@
 package me.typosbro.moshimoshi;
 
+import java.util.List;
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class MoshiMoshiApplication {
@@ -9,5 +13,14 @@ public class MoshiMoshiApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(MoshiMoshiApplication.class, args);
 	}
+
+	@Bean
+	CommandLineRunner runner(MovieRepository movieRepository){
+		return args -> {
+			Movie movie = new Movie("Kaguya-sama", List.of("Shounen", "Comedy", "Romance", "Drama"));
+			movieRepository.insert(movie);
+		};
+
+	};
 
 }
