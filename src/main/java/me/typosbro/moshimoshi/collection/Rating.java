31ca42784550ea67ee28ff1eac4c749eb03326af
Azizbek <khasanjonovich@gmail.com>
2023-05-09 08:17:07 +0900

    remove old code

--- src/main/java/me/typosbro/moshimoshi/collection/Rating.java
+++
@@ -1,8 +0,0 @@
-package me.typosbro.moshimoshi.collection;
-
-public class Rating {
-    private String user;
-    private String movie;
-    private Number rating;
-    private String timestamp;
-}
