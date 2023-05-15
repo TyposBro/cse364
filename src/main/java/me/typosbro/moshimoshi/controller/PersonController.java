90864aeba37561236014593172cc778136d76b77
Azizbek <khasanjonovich@gmail.com>
2023-05-15 22:53:10 +0900

    GET/POST/DELETE APIs

--- src/main/java/me/typosbro/moshimoshi/controller/PersonController.java
+++ src/main/java/me/typosbro/moshimoshi/controller/PersonController.java
@@ -1,9 +1,15 @@
 package me.typosbro.moshimoshi.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import me.typosbro.moshimoshi.collection.Person;
@@ -19,6 +25,22 @@ public class PersonController {
     @PostMapping
     public String save(@RequestBody Person person) {
         return personService.save(person);
+    }
+
+    @GetMapping
+    public List<Person> getPersonsStartWith(@RequestParam("name") String name) {
+        return personService.getPersonStartWith(name);
+
+    }
+
+    @GetMapping()
+    public List<Person> getAll() {
+        return personService.getAll();
+    }
+
+    @DeleteMapping("/{id}")
+    public String delete(@PathVariable String id) {
+        return personService.delete(id);
     }
 
 }
