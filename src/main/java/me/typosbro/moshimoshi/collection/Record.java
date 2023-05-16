2978deddea4272959b1356da9b976efa8520e112
Azizbek <khasanjonovich@gmail.com>
2023-05-16 09:55:00 +0900

    fix

--- src/main/java/me/typosbro/moshimoshi/collection/Record.java
+++ src/main/java/me/typosbro/moshimoshi/collection/Record.java
@@ -2,13 +2,13 @@ package me.typosbro.moshimoshi.collection;
 
 import java.util.List;
 
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 
 import lombok.Builder;
 import lombok.Data;
-import nonapi.io.github.classgraph.json.Id;
 
 @Data
 @Builder
