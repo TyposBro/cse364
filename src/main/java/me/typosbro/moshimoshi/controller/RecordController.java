07d08395b13174b95bf5107bd505adb9faa6c3c3
Azizbek <khasanjonovich@gmail.com>
2023-05-16 11:35:06 +0900

    fix import

--- src/main/java/me/typosbro/moshimoshi/controller/RecordController.java
+++ src/main/java/me/typosbro/moshimoshi/controller/RecordController.java
@@ -8,10 +8,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import me.typosbro.moshimoshi.collection.Record;
 import me.typosbro.moshimoshi.service.RecordService;
 
